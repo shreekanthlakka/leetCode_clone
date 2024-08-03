eae8e4f42584a743ceb355c9221c8ea5383a2d87
shreekanthlakka <shreekanthlakka@hotmail.com>
2024-08-03 10:24:01 +0530

    pro page in account page

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,5 +1,25 @@
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
+
+const Container = styled.div`
+    margin-top: 1rem;
+`;
+
 function Account() {
-    return <div>Account</div>;
+    const navigate = useNavigate();
+    return (
+        <Container>
+            <ButtonGroup variant="outlined" aria-label="Basic button group">
+                <Button>Account</Button>
+                <Button>Settings</Button>
+                <Button onClick={() => navigate("/account/pro")}>
+                    Pro Plan
+                </Button>
+            </ButtonGroup>
+        </Container>
+    );
 }
 
 export default Account;
