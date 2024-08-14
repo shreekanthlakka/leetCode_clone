cc3984b236169aad005fd31055f6ffd558589cf0
shreekanthlakka <shreekanthlakka@hotmail.com>
2024-08-14 23:49:22 +0530

    client side websocket

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,24 @@
 import { Divider } from "@mui/material";
 import Hero from "./Hero";
 import PopularProblems from "./PopularProblems";
-import { io } from "socket.io-client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
+    const [socket, setSocket] = useState(null);
     useEffect(() => {
-        const socket = io("https://leetcode.dev/ws");
+        const newSocket = new WebSocket("ws://leetcode-dev.store/ws");
+        newSocket.onopen = () => {
+            console.log("Connected to the server");
+            newSocket.send("Hello Server!");
+        };
+        newSocket.onmessage = (event) => {
+            console.log("Received message from server", event.data);
+        };
+        newSocket.onclose = () => {
+            console.log("Disconnected from the server");
+        };
+        setSocket(newSocket);
+        return () => newSocket.close();
     }, []);
     return (
         <div>
