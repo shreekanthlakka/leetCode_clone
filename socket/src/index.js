fdaa795019590fa2f80b48f316ddd3d82e9d9bd2
shreekanthlakka <shreekanthlakka@hotmail.com>
2024-08-14 16:21:05 +0530

    added more pages

--- socket/src/index.js
+++ socket/src/index.js
@@ -3,20 +3,8 @@ import WebSocket, { WebSocketServer } from "ws";
 import { natsWrapper } from "./nats-wrapper.js";
 import { socketWrapper } from "./socket-wrapper.js";
 import { JobCompletedStatusListener } from "./events/listeners/jobcompleted-status-listener.js";
-
-// const httpServer = app.listen(8000, () => {
-//     console.log("Server is running on port 8000");
-// });
-
-// const wss = new WebSocketServer({ server: httpServer });
-
-// wss.on("connection", (ws) => {
-//     console.log("New connection", ws);
-//     ws.on("message", (data) => {
-//         console.log(`Received message: ${data}`);
-//     });
-//     ws.send("Hello from server");
-// });
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const connectToNats = async () => {
     var count = 0;
@@ -27,6 +15,7 @@ const connectToNats = async () => {
             process.env.NATS_URL
         );
         console.log("Connected to NATS");
+        new JobCompletedStatusListener(natsWrapper.client).listen();
     } catch (error) {
         console.log(
             `error connecting to NATS , attempting ${count} th time !!`,
@@ -40,8 +29,6 @@ const connectToNats = async () => {
             console.log("NATS connection closed!!!");
             process.exit();
         });
-        //listener
-        new JobCompletedStatusListener(natsWrapper.client).listen();
         process.on("SIGINT", () => natsWrapper.client.close());
         process.on("SIGTERM", () => natsWrapper.client.close());
     }
@@ -58,26 +45,36 @@ const start = async () => {
         throw new Error("NATS Url not defined !!!");
     }
     try {
+        const wss = new WebSocketServer({ port: 3000 });
+        wss.on("connection", (ws) => {
+            console.log("New connection", ws);
+            ws.on("error", console.error);
+            ws.on("message", (data) => {
+                console.log(`Received message: ${data}`);
+            });
+            ws.send("Hello from server");
+        });
+
+        // const server = new createServer(app);
+        // const io = new Server(server, {
+        //     cors: {
+        //         origin: "*",
+        //     },
+        // });
+        // io.on("connection", (socket) => {
+        //     console.log("New connection", socket.id);
+        // });
+        // io.on("error", (error) => {
+        //     console.log("Error =>", error);
+        // });
+
+        // server.listen(3000, () => {
+        //     console.log("Socket server running on port 3000");
+        // });
+
         await connectToNats();
-        await socketWrapper.connect(3000);
-        console.log("Connected to Socket");
     } catch (error) {
         console.log("<== Error ==>", error.message);
-    } finally {
-        // const server = app.listen(3000, () => {
-        //     console.log("Websocket server is running on port 3000");
-        // });
-        // const wss = new WebSocketServer({ server });
-        // wss.on("connection", (ws) => {
-        //     console.log("New connection", ws);
-        //     ws.on("message", (data) => {
-        //         console.log(`Received message: ${data}`);
-        //     });
-        //     ws.send("Hello from server");
-        //     ws.on("error", (err) => {
-        //         console.log("Error connecting to websocket ==> ", err);
-        //     });
-        // });
     }
 };
 
